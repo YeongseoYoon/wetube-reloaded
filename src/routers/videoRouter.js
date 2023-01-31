3fb51c29df55acd2280b5c92fba3778c33f54479
root <root@DESKTOP-00PF5NQ>
2023-02-01 07:54:53 +0900

    Modify video update function to add validation

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -13,10 +13,8 @@ const videoRouter = express.Router();
 //cuz if some of users request for http://localhost:4000/videos/upload,
 //the router can be confused between real-upload and :id-upload
 //But if you use with regular expression, that kinda problems will be nothing
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.post("/:id(\\d+)/edit", postEdit);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
