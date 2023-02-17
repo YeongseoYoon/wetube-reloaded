252401c4f26245de802dc28c696d402f02158a5f
root <root@DESKTOP-00PF5NQ>
2023-02-17 18:25:29 +0900

    Design 💄:전체적인 scss추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,9 +6,9 @@ const HTTP_NOT_FOUND = 404;
 const HTTP_FORBIDDEN = 403;
 
 export const home = async(req,res)=> {
-    const videos = await Video.find({}).sort({
-      createdAt:"desc"
-    });
+  const videos = await Video.find({})
+  .sort({ createdAt: "desc" })
+  .populate("owner");
     return res.render("home",{pageTitle : "Home", videos});
 };
 export const watch = async(req, res) => {
@@ -110,7 +110,7 @@ export const search = async(req, res) =>{
       title:{
         $regex: new RegExp(keyword,"i")
       },
-    });
+    }).populate("owner");
   }
   return res.render("search",{pageTitle : "Search",videos});
 }
