6b631e30a6c381f8a99cd31d876ccfd6e2df4e04
root <root@DESKTOP-00PF5NQ>
2023-03-07 02:47:51 +0900

    For test

--- src/init.js
+++ src/init.js
@@ -8,7 +8,7 @@ import app from "./server";
 
 //define port number
 const PORT = process.env.PORT || 4000;
-
+console.log("이건" + process.env.NODE_ENV);
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 app.listen(4000, handleListening);
