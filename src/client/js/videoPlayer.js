12a8ba959f9f4bc366cc79e3ec6a5f9350d351e0
root <root@DESKTOP-00PF5NQ>
2023-03-06 20:41:00 +0900

    :art: Fix: 영상 길이 받아 오지 못하는 문제 수정
    Fix: 영상 길이 받아 오지 못하는 문제 수정

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -17,123 +17,130 @@ let controlsTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
-const handlePlayClick = (e) =>{
-    if(video.paused){
-        video.play();
-    }else{
-        video.pause();
-    }
-    playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handlePause = () => (playBtn.innerText = "Play");
 const handlePlay = () => (playBtn.innerText = "Pause");
 
-const handleMute = (e) =>{
-    if(video.muted){
-        video.muted = false;
-    }else{
-        video.muted = true;
-    }
-    muteBtnIcon.classList = video.muted
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtnIcon.classList = video.muted
     ? "fas fa-volume-mute"
     : "fas fa-volume-up";
-    volumeRange.value = video.muted ? 0 : volumeValue;
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
-const handleVolumeChange= (event) =>{
-    const {target:{value}} = event;
-    if(video.muted){
-        video.muted = false;
-        muteBtnIcon.classList = "fas fa-volume-up"; 
-    }
-    if (value == 0) {muteBtnIcon.classList = "fas fa-volume-mute"; }
-    volumeValue = value;
-    video.volume = value;
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtnIcon.classList = "fas fa-volume-up";
+  }
+  if (value == 0) {
+    muteBtnIcon.classList = "fas fa-volume-mute";
+  }
+  volumeValue = value;
+  video.volume = value;
 };
 
-const formatTime = (seconds) => new Date(seconds*1000).toISOString().substring(11, 19);
+const formatTime = (seconds) =>
+  new Date(seconds * 1000).toISOString().substring(11, 19);
 
-const handleLoadedMetadata = () =>{
-    totalTime.innerText = formatTime(Math.floor(video.duration));
-    timeline.max = Math.floor(video.duration); 
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
 };
 
-const handleTimeUpdate = () =>{
-    currentTime.innerText=formatTime(Math.floor(video.currentTime));
-    timeline.value = Math.floor(video.currentTime);
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
 };
 
 const handleTimelineChange = (event) => {
-    const {target: {value}} = event;
-    video.currentTime = value;
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
 };
 
 const handleFullscreen = () => {
-    const fullscreen = document.fullscreenElement;
-    if (fullscreen) {
-      document.exitFullscreen();
-      fullScreenIcon.classList = "fas fa-expand";
-    } else {
-      videoContainer.requestFullscreen();
-      fullScreenIcon.classList = "fas fa-compress";
-    }
-  };
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    document.exitFullscreen();
+    fullScreenIcon.classList = "fas fa-expand";
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenIcon.classList = "fas fa-compress";
+  }
+};
 
-  const hideControls = () => videoControls.classList.remove("showing");
-  
-  const handleMouseMove = () => {
-    if(controlsTimeout){
-        clearTimeout(controlsTimeout);
-        controlsTimeout = null;
-    }
-    if(controlsMovementTimeout){
-        clearTimeout(controlsMovementTimeout);
-        controlsMovementTimeout = null;
-    }
-    videoControls.classList.add("showing");
-    //움직일때마다 계속해서 타임아웃을 줌
-    //안움직이면 타임아웃이 없을것임! hideControls만 불려졌을것
-    controlsMovementTimeout = setTimeout(hideControls,3000);
-  };
+const hideControls = () => videoControls.classList.remove("showing");
 
-  const handleMouseLeave = () =>{
-    controlsTimeout = setTimeout(hideControls,3000);
+const handleMouseMove = () => {
+  if (controlsTimeout) {
     clearTimeout(controlsTimeout);
-  };
+    controlsTimeout = null;
+  }
+  if (controlsMovementTimeout) {
+    clearTimeout(controlsMovementTimeout);
+    controlsMovementTimeout = null;
+  }
+  videoControls.classList.add("showing");
+  //움직일때마다 계속해서 타임아웃을 줌
+  //안움직이면 타임아웃이 없을것임! hideControls만 불려졌을것
+  controlsMovementTimeout = setTimeout(hideControls, 3000);
+};
 
-  const changeVideoTime = (seconds) => {
-    video.currentTime += seconds;
-    };
-    
-    document.addEventListener("keyup", (event) => {
-        if (event.code === "Space") {
-            handlePlayClick();
-            event.preventDefault();
-        }
-        if (event.code === "ArrowRight") {
-            changeVideoTime(5);
-        }
-        if (event.code === "ArrowLeft") {
-            changeVideoTime(-5);
-        }
-        if (event.code === "KeyM") {
-            handleMute();
-        }
-    });
+const handleMouseLeave = () => {
+  controlsTimeout = setTimeout(hideControls, 3000);
+  clearTimeout(controlsTimeout);
+};
 
-    const handleEnded = async() => {
-        const { id } = videoContainer.dataset;
-        await fetch(`/api/videos/${id}/view`, {
-          method: "POST",
-        });
-      };
-playBtn.addEventListener("click",handlePlayClick);
-muteBtn.addEventListener("click",handleMute);
-volumeRange.addEventListener("input",handleVolumeChange);
-video.addEventListener("loadeddata", handleLoadedMetadata);
+const changeVideoTime = (seconds) => {
+  video.currentTime += seconds;
+};
+
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    handlePlayClick();
+    event.preventDefault();
+  }
+  if (event.code === "ArrowRight") {
+    changeVideoTime(5);
+  }
+  if (event.code === "ArrowLeft") {
+    changeVideoTime(-5);
+  }
+  if (event.code === "KeyM") {
+    handleMute();
+  }
+});
+
+const handleEnded = async () => {
+  const { id } = videoContainer.dataset;
+  await fetch(`/api/videos/${id}/view`, {
+    method: "POST",
+  });
+};
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("canplay", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
-video.addEventListener("ended",handleEnded);
+video.addEventListener("ended", handleEnded);
 video.addEventListener("click", handlePlayClick);
 videoContainer.addEventListener("mousemove", handleMouseMove);
 videoContainer.addEventListener("mouseleave", handleMouseLeave);
